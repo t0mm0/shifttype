829c2922cb4f21cfee27a2dc41f8ffe65d1e776e
t0mmm0 <t0mmm0@onairsoftware.co.uk>
2020-03-28 15:02:25 +0000

    Add main basic menu screen
    also handle back button

--- main.py
+++ main.py
@@ -2,18 +2,34 @@ import kivy
 kivy.require('1.11.0')
 
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.logger import Logger
+from kivy.uix.screenmanager import ScreenManager
 import logging
 
 
 Logger.setLevel(logging.DEBUG)
 
 
+class TsScreenManager(ScreenManager):
+    def go_back(self):
+        Logger.debug(f"go_back from: {self.current}")
+        if self.current == 'game':
+            self.transition.direction = 'right'
+            self.current = 'menu'
+            return True
+
 class MainApp(App):
     def __init__(self, **kwargs):
         self.title = "shiftTYPE"
         super().__init__(**kwargs)
-    pass
+        Window.bind(on_keyboard=self.on_keypress)
+    
+    def on_keypress(self, window, key, *args):
+        # handle back button
+        if key == 27:
+            return self.root.go_back()
+
 
 
 if __name__ == '__main__':
